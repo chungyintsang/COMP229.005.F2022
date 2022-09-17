f0a2dd9c1c81f72fae10a0271c75e6e51580f888
Chung Yin Tsang <113803668+chungyintsang@users.noreply.github.com>
2022-09-17 19:51:54 -0400

    Arrow 1

---
+++ ArrowFunction/index.js
@@ -0,0 +1,14 @@
+var hello;
+
+hello = () => {
+    document.getElementById("demo").innerHTML += "Hello 1";
+}
+
+
+//A button object calls the function:
+document.getElementById("btn1").addEventListener("click", hello);
+
+//A button object calls the function:
+document.getElementById("btn2").addEventListener("click", () => {
+    document.getElementById("demo").innerHTML += "Hello 2";
+})
