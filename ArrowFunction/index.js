88b9593beb3b44525e8313dae87a7d828c5fc8ea
Chung Yin Tsang <113803668+chungyintsang@users.noreply.github.com>
2022-09-17 20:19:56 -0400

    DRS and LetAndConst

--- ArrowFunction/index.js
+++ ArrowFunction/index.js
@@ -1,14 +1,53 @@
-var hello;
+//var hello;
 
-hello = () => {
-    document.getElementById("demo").innerHTML += "Hello 1";
-}
+//hello = () => {
+//    document.getElementById("demo").innerHTML += "Hello 1";
+//}
 
-
-//A button object calls the function:
-document.getElementById("btn1").addEventListener("click", hello);
 
 //A button object calls the function:
-document.getElementById("btn2").addEventListener("click", () => {
-    document.getElementById("demo").innerHTML += "Hello 2";
-})
+//document.getElementById("btn1").addEventListener("click", hello);
+
+//A button object calls the function:
+//document.getElementById("btn2").addEventListener("click", () => {
+//    document.getElementById("demo").innerHTML += "Hello 2";
+//})
+
+//var numbers = [1, 2, 3, 4, 5];
+//const squares = numbers.map(n => n * n);
+//document.write(squares);
+
+//var evens = new Array();
+//numbers.forEach(n => {
+//    if (n % 2 === 0){
+//        evens.push(n);
+//    }
+//});
+//document.write('<br>');
+//document.write(evens);
+
+const author = {
+    fullName: "Bob Alice",
+    books: ["Book 01", "Book 02", "Book 03"],
+    printBooks() {
+        this.books.forEach(book => console.log(book + ' by ' + this.fullName + '<br>'));
+    },
+
+    printBooks1() {
+        function printValue(book){
+            document.write(book + ' by ' + this.fullName + '<br>'); // Undefined expected for 
+        }
+
+        this.books.forEach(printValue);
+    },
+
+    printBooks2(){
+        this.books.forEach(function(book){
+            document.write(book + ' by ' +this.fullName + '<br>');
+        })
+    }
+};
+
+author.printBooks();
+author.printBooks1();
+author.printBooks2();
