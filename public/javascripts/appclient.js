7d01d1d2a203d5202ae75d1029755f6a569daeb1
Chung Yin Tsang <113803668+chungyintsang@users.noreply.github.com>
2022-10-07 18:34:34 -0400

    Adds comments(name, ID, Date)

--- public/javascripts/appclient.js
+++ public/javascripts/appclient.js
@@ -1,1 +1,5 @@
+// Student ID: 301216704
+// Name: Chung Yin Tsang
+// Date: 7 Oct 2022
+
 console.log("It goes to the client-side.")
--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,6 @@
+// Student ID: 301216704
+// Name: Chung Yin Tsang
+// Date: 7 Oct 2022
 var express = require('express');
 var router = express.Router();
 
