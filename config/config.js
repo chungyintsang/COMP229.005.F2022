11fff76e2086c1085e4eae0663991a3c25c00b7d
Chung Yin Tsang <113803668+chungyintsang@users.noreply.github.com>
2022-11-23 18:20:21 -0500

    Week 07 - final (jwt, token)

---
+++ config/config.js
@@ -0,0 +1,8 @@
+// Do not expose your credentials in your code.
+
+module.exports = {
+    "ATLASDB": "mongodb+srv://dbuser:Ny3UVBOD3EO3oZpK@cluster005.8scysba.mongodb.net/products?retryWrites=true&w=majority",
+    "LOCALDB": "mongodb://localhost:27017/dbapp",
+    //API key
+    "SECRETKEY": "y$B&E)H+MbQeThWmZq4t7w!z%C*F-JaNcRfUjXn2r5u8x/A?D(G+KbPeSgVkYp3s"
+}
--- config/db.js
+++ config/db.js
@@ -1,13 +1,10 @@
-// In real project, never expose your credential in the code.
-
-let atlasDB = "mongodb+srv://dbuser:Ny3UVBOD3EO3oZpK@cluster005.8scysba.mongodb.net/products?retryWrites=true&w=majority";
-
+let config = require('./config');
 let mongoose = require('mongoose');
 
 module.exports = function(){
 
     //Connect to the database
-    mongoose.connect(atlasDB);
+    mongoose.connect(config.ATLASDB);
 
     let mongodb = mongoose.connection;
     mongodb.on('error', console.error.bind(console, 'Connection Error:'));
