594e84c517e75160d51266b5659e1d13a7b5f1fb
Chung Yin Tsang <113803668+chungyintsang@users.noreply.github.com>
2022-11-24 05:28:21 -0500

    Adds try-catch, modifiedCount

--- controllers/inventory.js
+++ controllers/inventory.js
@@ -40,65 +40,96 @@ module.exports.inventoryList = async function(req, res, next) {
 
 module.exports.processEdit = (req, res, next) => {
 
-    let id = req.params.id
+    try {
+        let id = req.params.id;
 
-    let updatedItem = InventoryModel({
-        _id: id,
-        item: req.body.item,
-        qty: req.body.qty,
-        status: req.body.status,
-        size : {
-            h: req.body.size.h,
-            w: req.body.size.w,
-            uom: req.body.size.uom,
-        },
-        tags: (req.body.tags == null || req.body.tags == "" )? "": req.body.tags.split(",").map(word => word.trim()),
-        owner: (req.body.owner == null || req.body.owner == "")? req.payload.id : req.body.owner
-    });
-
-    InventoryModel.updateOne({_id: id}, updatedItem, (err) => {
-        if(err)
-        {
-            console.log(err);
-            res.status(400).json({
-                success: false,
-                message: getErrorMessage(err)
-            });
-        }
-        else
-        {
-            res.status(200).json({
-                success: true,
-                message: "Item updated sucessfully."
-            });
-        }
-    });
+        let updatedItem = InventoryModel({
+            _id: id,
+            item: req.body.item,
+            qty: req.body.qty,
+            status: req.body.status,
+            size : {
+                h: req.body.size.h,
+                w: req.body.size.w,
+                uom: req.body.size.uom,
+            },
+            tags: (req.body.tags == null || req.body.tags == "") ? "": req.body.tags.split(",").map(word => word.trim()),
+            // If it does not have an owner it assumes the ownership otherwise it transfers it.
+            owner: (req.body.owner == null || req.body.owner == "")? req.payload.id : req.body.owner 
+        });
+    
+        InventoryModel.updateOne({_id: id}, updatedItem, (err, result) => {
+            console.log(err, result);
+            // see the number of modifiedcount. if 0, no data is being modified
+            if(err || result.modifiedCount == 0)
+            {
+                console.log(err);
+ 
+                return res.status(400).json(
+                    { 
+                        success: false, 
+                        message: err ? getErrorMessage(err): 'Item Not Found.'
+                    }
+                );
+            }
+            else
+            {
+                res.status(200).json(
+                    {
+                        success: true,
+                        message: 'Item updated successfully.'
+                    }
+                )
+            }
+        });
+    } catch (error) {
+        return res.status(400).json(
+            { 
+                success: false, 
+                message: getErrorMessage(error)
+            }
+        );
+    }
 
 }
 
 
 module.exports.performDelete = (req, res, next) => {
 
-    let id = req.params.id;
+    try {
+        let id = req.params.id;
 
-
-    InventoryModel.remove({_id: id}, (err) => {
-        if(err)
-        {
-            console.log(err);
-            res.status(400).json({
-                success: false,
-                message: getErrorMessage(err)
-            });
-        }
-        else
-        {
-            res.status(200).json({
-                success: true,
-                message: "Item removed sucessfully."
-            });
-        }
-    });
+        InventoryModel.findByIdAndRemove({_id: id}, {rawResult:true}, (err, result) => {
+            console.log(err, result);
+            if(err || result.value == null)
+            {
+                console.log(err);
+ 
+                return res.status(400).json(
+                    { 
+                        success: false, 
+                        message: err ? getErrorMessage(err): 'Item Not Found.'
+                    }
+                );
+            }
+            else
+            {
+                res.status(200).json(
+                    {
+                        success: true,
+                        message: 'Item deleted successfully.'
+                    }
+                )
+            }
+        });
+    } catch (error) {
+        return res.status(400).json(
+            { 
+                success: false, 
+                message: getErrorMessage(error)
+            }
+        );
+    }
 
 }
 
@@ -106,37 +137,45 @@ module.exports.performDelete = (req, res, next) => {
 // Backend: No display add page
 
 module.exports.processAdd = (req, res, next) => {
+    try {
+        let newItem = InventoryModel({
+            _id: req.body.id,
+            item: req.body.item,
+            qty: req.body.qty,
+            status: req.body.status,
+            size : {
+                h: req.body.size.h,
+                w: req.body.size.w,
+                uom: req.body.size.uom,
+            },
+            tags: (req.body.tags == null || req.body.tags == "" )? "": req.body.tags.split(",").map(word => word.trim()),
+            owner: (req.body.owner == null || req.body.owner == "")? req.payload.id : req.body.owner
+        });
 
-    let newItem = InventoryModel({
-        _id: req.body.id,
-        item: req.body.item,
-        qty: req.body.qty,
-        status: req.body.status,
-        size : {
-            h: req.body.size.h,
-            w: req.body.size.w,
-            uom: req.body.size.uom,
-        },
-        tags: (req.body.tags == null || req.body.tags == "" )? "": req.body.tags.split(",").map(word => word.trim()),
-        owner: (req.body.owner == null || req.body.owner == "")? req.payload.id : req.body.owner
-    });
-
-    InventoryModel.create(newItem, (err, item) =>{
-        if(err)
-        {
-            console.log(err);
-            res.status(400).json({
-                success: false,
-                message: getErrorMessage(err)
-            });
-        }
-        else
-        {
-            // refresh the book list
-            console.log(item);
-            // Backend: no rediret
-            res.status(200).json(item);
-        }
-    });
+        InventoryModel.create(newItem, (err, item) =>{
+            if(err)
+            {
+                console.log(err);
+                res.status(400).json({
+                    success: false,
+                    message: getErrorMessage(err)
+                });
+            }
+            else
+            {
+                // refresh the book list
+                console.log(item);
+                // Backend: no rediret
+                res.status(200).json(item);
+            }
+        });
+    } catch (error) {
+        return res.status(400).json(
+            { 
+                success: false, 
+                message: getErrorMessage(error)
+            }
+        );
+    }  
     
 }
